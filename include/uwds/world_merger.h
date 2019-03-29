8a6cea39f729a680b828438dc20b22cb99aa60ff
Yoan Sallami <ysallami@tully.laas.fr>
2019-03-29 15:12:39 +0100

    :sparkles: Add world_merger 	modified:   CMakeLists.txt 	modified:   include/uwds/world_merger.h 	modified:   package.xml 	modified:   src/world_merger.cpp

--- include/uwds/world_merger.h
+++ include/uwds/world_merger.h
@@ -2,14 +2,65 @@
 #define WORLD_MERGER_HPP
 
 #include "uwds/reconfigurable_client.h"
+#include <tf2_ros/transform_listener.h>
+#include <tf2/transform_datatypes.h>
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include <geometry_msgs/TransformStamped.h>
+#include <pose_cov_ops/pose_cov_ops.h>
 
 using namespace std;
 using namespace std_msgs;
 using namespace uwds_msgs;
 
+typedef boost::shared_ptr<tf2_ros::Buffer> BufferPtr;
+typedef boost::shared_ptr<tf2_ros::TransformListener> TransformListenerPtr;
+
 namespace uwds
 {
+  class WorldMerger : public uwds::ReconfigurableClient
+  {
+  public:
+    /**@brief
+     * The default constructor.
+     */
+    WorldMerger(): uwds::ReconfigurableClient(uwds::FILTER) {}
 
+    /**@brief
+     * The default destructor
+     */
+    ~WorldMerger() = default;
+
+    /** @brief
+     * Initialize method. Subclass should call this method
+     * in its onInit method.
+     */
+    virtual void onInit();
+
+  protected:
+
+    /** @brief
+     * This method is called when there is a change in a world.
+     *
+     * @param world The world that have been updated
+     * @param header The header
+     * @param invalidations The invalidations received
+     */
+    virtual void onChanges(const string& world,
+                           const Header& header,
+                           const Invalidations& invalidations);
+
+    virtual void onReconfigure(const std::vector<std::string>& worlds) {}
+
+    void onTimer(const ros::TimerEvent& event);
+
+    mutex changes_mutex_;
+
+    Changes changes_to_send_;
+
+    TransformListenerPtr tf_listener_;
+
+    BufferPtr tf_buffer_;
+  };
 }
 
 #endif
--- src/world_merger.cpp
+++ src/world_merger.cpp
@@ -6,5 +6,96 @@ using namespace uwds_msgs;
 
 namespace uwds
 {
+  void WorldMerger::onInit()
+  {
+    tf_buffer_ = boost::make_shared<tf2_ros::Buffer>();
+    tf_listener_ = boost::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    ReconfigurableClient::onInit();
+  }
 
+  void WorldMerger::onChanges(const string& world,
+                              const Header& header,
+                              const Invalidations& invalidations)
+  {
+    auto& scene = ctx_->worlds()[world].scene();
+    auto& timeline = ctx_->worlds()[world].timeline();
+    auto& meshes = ctx_->worlds()[world].meshes();
+
+    Changes changes;
+
+    changes_mutex_.lock();
+    for(const string& id : invalidations.node_ids_updated)
+    {
+      if (scene.nodes()[id].name != "root")
+      {
+        bool insert = false;
+        bool transformed = false;
+        //
+        if(header.frame_id != global_frame_id_)
+        {
+          geometry_msgs::TransformStamped transformStamped;
+          tf2::Stamped<tf2::Transform> temp;
+          geometry_msgs::PoseStamped sensor_pose;
+          try {
+            transformStamped = tf_buffer_->lookupTransform(header.frame_id, global_frame_id_, ros::Time(0));
+            tf2::fromMsg(transformStamped, temp);
+            tf2::toMsg(temp, sensor_pose);
+            pose_cov_ops::compose(sensor_pose.pose, scene.nodes()[id].position.pose, scene.nodes()[id].position.pose);
+            transformed = true;
+          } catch(tf2::TransformException& e) {
+            ROS_WARN("[%s::onChanges] Error occured while transforming node into world frame : %s",ctx_->name().c_str(), e.what());
+          }
+        } else {
+          transformed = true;
+        }
+        if (transformed)
+        {
+          for (uint i=0; i < changes_to_send_.nodes_to_update.size(); ++i) {
+            if (changes_to_send_.nodes_to_update[i].id == id)
+            {
+              changes_to_send_.nodes_to_update[i] = scene.nodes()[id];
+            }
+          }
+          if(!insert) changes_to_send_.nodes_to_update.push_back(scene.nodes()[id]);
+        }
+      }
+    }
+    for(const string& id : invalidations.situation_ids_updated)
+    {
+      bool insert = false;
+      for (uint i=0; i < changes_to_send_.situations_to_update.size(); ++i) {
+        if (changes_to_send_.situations_to_update[i].id == id)
+        {
+          changes_to_send_.situations_to_update[i] = timeline.situations()[id];
+        }
+      }
+      if(!insert) changes_to_send_.situations_to_update.push_back(timeline.situations()[id]);
+    }
+    for(const std::string& id : invalidations.mesh_ids_updated)
+    {
+      bool insert = false;
+      for (uint i=0; i < changes.meshes_to_update.size(); ++i) {
+        if (changes.meshes_to_update[i].id == id)
+        {
+          changes_to_send_.meshes_to_update[i] = meshes[id];
+        }
+      }
+      if(!insert) changes_to_send_.meshes_to_update.push_back(meshes[id]);
+    }
+    changes_mutex_.unlock();
+  }
+
+  void WorldMerger::onTimer(const ros::TimerEvent& event) {
+    for (const auto& world : input_worlds_)
+    {
+      Header header;
+      header.stamp = ros::Time::now();
+      header.frame_id = global_frame_id_;
+      ctx_->worlds()[output_world_].update(header, changes_to_send_);
+      if(verbose_)NODELET_INFO("[%s::onChanges] Send changes to world <%s>", ctx_->name().c_str(), output_world_.c_str());
+    }
+  }
 }
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(uwds::WorldMerger, nodelet::Nodelet)
