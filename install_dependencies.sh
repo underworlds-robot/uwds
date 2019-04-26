a37464b8198228d3a875e7be4b893f3ff79a1199
Yoan Sallami <ysallami@laas.fr>
2019-04-26 13:46:11 +0200

    :ambulance: fix install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-apt-get install --assume-yes --user assimp-utils ros-$ROS_DISTRO-pose-cov-ops ros-$ROS_DISTRO-jsk-recognition-msgs ros-$ROS_DISTRO-jsk-visualization-msgs ros-$ROS_DISTRO-jsk-visualization ros-$ROS_DISTRO-ar-track-alvar ros-$ROS_DISTRO-octomap ros-$ROS_DISTRO-doc-lite
+echo "Start downloding dependencies... this may take a while..."
+sudo apt-get install --assume-yes assimp-utils ros-$ROS_DISTRO-pose-cov-ops ros-$ROS_DISTRO-jsk-recognition-msgs ros-$ROS_DISTRO-jsk-visualization-msgs ros-$ROS_DISTRO-jsk-visualization ros-$ROS_DISTRO-ar-track-alvar ros-$ROS_DISTRO-octomap ros-$ROS_DISTRO-doc-lite
 pip install -r requirements.txt
 cd ..
 git clone https://github.com/underworlds-robot/uwds_msgs.git
-git clone https://github.com/underworlds-robot/uwds_physics_clients.git
 cd -
 cd ~
 git clone https://github.com/bulletphysics/bullet3.git
-python ./bullet3/setup.py install --prefix --prefix=${HOME}
+python ./bullet3/setup.py install --prefix=${HOME}
 cd -
-cd ../..
-catkin_make --pkg uwds
-cd -
+echo "Bye bye !"
