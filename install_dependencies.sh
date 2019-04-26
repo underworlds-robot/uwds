32736a8ebd6265262fabb0cd855b165d09c10f5a
Yoan Sallami <ysallami@laas.fr>
2019-04-26 13:59:15 +0200

    :ambulance: fix install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Start downloding dependencies... this may take a while..."
-sudo apt-get install --assume-yes assimp-utils ros-$ROS_DISTRO-pose-cov-ops ros-$ROS_DISTRO-jsk-recognition-msgs ros-$ROS_DISTRO-jsk-visualization-msgs ros-$ROS_DISTRO-jsk-visualization ros-$ROS_DISTRO-ar-track-alvar ros-$ROS_DISTRO-octomap ros-$ROS_DISTRO-doc-lite
+sudo apt-get install --assume-yes assimp-utils ros-$ROS_DISTRO-pose-cov-ops ros-$ROS_DISTRO-jsk-recognition-msgs ros-$ROS_DISTRO-jsk-visualization ros-$ROS_DISTRO-ar-track-alvar ros-$ROS_DISTRO-octomap ros-$ROS_DISTRO-rosdoc-lite
 pip install -r requirements.txt
 cd ..
 git clone https://github.com/underworlds-robot/uwds_msgs.git
