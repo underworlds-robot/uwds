af42ed99978016c5c48b1d3d911edd7efc917fb8
Yoan Sallami <ysallami@laas.fr>
2019-04-26 14:49:13 +0200

    :ambulance: fix install stuff and server launchfile

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo "Start downloding dependencies... this may take a while..."
-sudo apt-get install --assume-yes assimp-utils ros-$ROS_DISTRO-pose-cov-ops ros-$ROS_DISTRO-jsk-recognition-msgs ros-$ROS_DISTRO-jsk-visualization ros-$ROS_DISTRO-ar-track-alvar ros-$ROS_DISTRO-octomap ros-$ROS_DISTRO-rosdoc-lite
+sudo apt-get install --assume-yes libbullet-dev assimp-utils ros-$ROS_DISTRO-pose-cov-ops ros-$ROS_DISTRO-jsk-recognition-msgs ros-$ROS_DISTRO-jsk-visualization ros-$ROS_DISTRO-ar-track-alvar ros-$ROS_DISTRO-octomap ros-$ROS_DISTRO-rosdoc-lite
 pip install -r requirements.txt --user
 cd ..
 git clone https://github.com/underworlds-robot/uwds_msgs.git
-cd -
-cd ~
-git clone https://github.com/bulletphysics/bullet3.git
-python ./bullet3/setup.py install --prefix=${HOME}
 cd -
 echo "Bye bye !"
